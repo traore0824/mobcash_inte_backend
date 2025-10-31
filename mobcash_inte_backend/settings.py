677cf076100e91aa4a14134189f7eca4ef7a7a06
Cascade <cascade@example.com>
2025-10-31 10:17:51 +0100

    add caisse in admin

--- mobcash_inte_backend/settings.py
+++ mobcash_inte_backend/settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["api.turaincash.com", "127.0.0.1", "api.turaincash.net"]
+ALLOWED_HOSTS = [
+    "api.turaincash.com",
+    "127.0.0.1",
+    "api.turaincash.net",
+    "api.zefast.net",
+]
 
 
 # Application definition
