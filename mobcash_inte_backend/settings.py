05745ac560fa59a7ec0c420c38f3b53d184bdf59
Cascade <cascade@example.com>
2025-11-07 11:48:14 +0100

    update url zefast

--- mobcash_inte_backend/settings.py
+++ mobcash_inte_backend/settings.py
@@ -86,6 +86,9 @@ CORS_ALLOWED_ORIGINS = [
     "https://turaincash.net",
     "https://admin.turaincash.net",
     "https://turnaicash-mobile-app-1.vercel.app",
+    "https://admin.zefast.net",
+    "https://zefast.net",
+    "https://zefast-mobile-app.vercel.app",
 ]
 
 
