616741fda521786a2e5bfde78f7c3f91eea81534
Cascade <cascade@example.com>
2025-11-22 11:10:25 +0100

    update

--- mobcash_inte_backend/settings.py
+++ mobcash_inte_backend/settings.py
@@ -519,7 +519,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "turaincash225@gmail.com"
-EMAIL_HOST_PASSWORD = os.getenv("BETPAY_EMAIL_PASSWORD")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
