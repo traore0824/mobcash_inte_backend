14ca41fa186b3a0030a2242e278695ae930637ee
Cascade <cascade@example.com>
2025-11-27 22:48:04 +0100

    update

--- mobcash_inte/apps.py
+++ mobcash_inte/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class MobcashInteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mobcash_inte'
+    def ready(self):
+        import mobcash_inte.signals
