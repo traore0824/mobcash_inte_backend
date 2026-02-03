2c157da502114995a6db6ee5ff61903d7e9b8f3e
Cascade <cascade@example.com>
2026-02-03 19:01:11 +0100

    update

--- mobcash_inte_backend/settings.py
+++ mobcash_inte_backend/settings.py
@@ -515,7 +515,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": True,
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "BLACKLIST_AFTER_ROTATION": True,
     "BLACKLIST_AFTER_ROTATION": True,
