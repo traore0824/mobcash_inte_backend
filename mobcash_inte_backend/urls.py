9d81202605d81717dea6c92ca9e5ebfa97e0a90a
Cascade <aliloulayei@gmail.com>
2026-03-18 14:56:11 +0100

    update partner

--- mobcash_inte_backend/urls.py
+++ mobcash_inte_backend/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("mobcash/", include("mobcash_inte.urls")),
+    path("blaffa/", include("mobcash_inte.urls")),
     path("auth/", include("accounts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
